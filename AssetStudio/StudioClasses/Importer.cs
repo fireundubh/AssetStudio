4f58ff79bee11eddd93bf283d222fbc44c31a444
fireundubh <fireundubh@gmail.com>
2018-11-13 16:22:09 -0800

    Cleanup Importer class

--- AssetStudio/StudioClasses/Importer.cs
+++ AssetStudio/StudioClasses/Importer.cs
@@ -1,14 +1,14 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 using static AssetStudio.Studio;
 
 namespace AssetStudio
 {
-    static class Importer
+    internal static class Importer
     {
         public static List<string> importFiles = new List<string>(); //files to load
         public static HashSet<string> importFilesHash = new HashSet<string>(); //to improve the loading speed
@@ -16,7 +16,7 @@ namespace AssetStudio
 
         public static void LoadFile(string fullName)
         {
-            switch (CheckFileType(fullName, out var reader))
+            switch (CheckFileType(fullName, out EndianBinaryReader reader))
             {
                 case FileType.AssetsFile:
                     LoadAssetsFile(fullName, reader);
@@ -32,114 +32,150 @@ namespace AssetStudio
 
         private static void LoadAssetsFile(string fullName, EndianBinaryReader reader, string parentPath = null)
         {
-            var fileName = Path.GetFileName(fullName);
+            string fileName = Path.GetFileName(fullName);
+
             StatusStripUpdate("Loading " + fileName);
-            if (!assetsfileListHash.Contains(fileName.ToUpper()))
+
+            Debug.Assert(fileName != null, nameof(fileName) + " != null");
+
+            if (assetsfileListHash.Contains(fileName.ToUpper()))
             {
-                var assetsFile = new AssetsFile(fullName, reader);
-                if (assetsFile.valid)
+                return;
+            }
+
+            var assetsFile = new AssetsFile(fullName, reader);
+
+            if (assetsFile.valid)
+            {
+                assetsFile.parentPath = parentPath;
+                assetsFileList.Add(assetsFile);
+                assetsfileListHash.Add(assetsFile.upperFileName);
+
+                #region for 2.6.x find mainData and get string version
+
+                if (assetsFile.header.m_Version == 6 && fileName != "mainData")
                 {
-                    assetsFile.parentPath = parentPath;
-                    assetsfileList.Add(assetsFile);
-                    assetsfileListHash.Add(assetsFile.upperFileName);
+                    AssetsFile mainDataFile = assetsFileList.Find(aFile => aFile.fileName == "mainData");
 
-                    #region for 2.6.x find mainData and get string version
-                    if (assetsFile.header.m_Version == 6 && fileName != "mainData")
+                    if (mainDataFile != null)
                     {
-                        var mainDataFile = assetsfileList.Find(aFile => aFile.fileName == "mainData");
-                        if (mainDataFile != null)
-                        {
-                            assetsFile.unityVersion = mainDataFile.unityVersion;
-                            assetsFile.version = mainDataFile.version;
-                            assetsFile.buildType = mainDataFile.buildType;
-                        }
-                        else if (File.Exists(Path.GetDirectoryName(fullName) + "\\mainData"))
-                        {
-                            mainDataFile = new AssetsFile(Path.GetDirectoryName(fullName) + "\\mainData", new EndianBinaryReader(File.OpenRead(Path.GetDirectoryName(fullName) + "\\mainData")));
-                            assetsFile.unityVersion = mainDataFile.unityVersion;
-                            assetsFile.version = mainDataFile.version;
-                            assetsFile.buildType = mainDataFile.buildType;
-                        }
+                        assetsFile.unityVersion = mainDataFile.unityVersion;
+                        assetsFile.version = mainDataFile.version;
+                        assetsFile.buildType = mainDataFile.buildType;
                     }
-                    #endregion
-
-                    int value = 0;
-                    foreach (var sharedFile in assetsFile.m_Externals)
+                    else if (File.Exists(Path.GetDirectoryName(fullName) + "\\mainData"))
                     {
-                        var sharedFilePath = Path.GetDirectoryName(fullName) + "\\" + sharedFile.fileName;
-                        var sharedFileName = sharedFile.fileName;
-
-                        if (!importFilesHash.Contains(sharedFileName.ToUpper()))
-                        {
-                            if (!File.Exists(sharedFilePath))
-                            {
-                                var findFiles = Directory.GetFiles(Path.GetDirectoryName(fullName), sharedFileName, SearchOption.AllDirectories);
-                                if (findFiles.Length > 0)
-                                {
-                                    sharedFilePath = findFiles[0];
-                                }
-                            }
-
-                            if (File.Exists(sharedFilePath))
-                            {
-                                importFiles.Add(sharedFilePath);
-                                importFilesHash.Add(sharedFileName.ToUpper());
-                                value++;
-                            }
-                        }
+                        mainDataFile = new AssetsFile(Path.GetDirectoryName(fullName) + "\\mainData", new EndianBinaryReader(File.OpenRead(Path.GetDirectoryName(fullName) + "\\mainData")));
+                        assetsFile.unityVersion = mainDataFile.unityVersion;
+                        assetsFile.version = mainDataFile.version;
+                        assetsFile.buildType = mainDataFile.buildType;
                     }
-                    if (value > 0)
-                        ProgressBarMaximumAdd(value);
                 }
-                else
-                    reader.Dispose();
+
+                #endregion
+
+                var value = 0;
+
+                foreach (FileIdentifier sharedFile in assetsFile.m_Externals)
+                {
+                    string sharedFilePath = Path.GetDirectoryName(fullName) + "\\" + sharedFile.fileName;
+                    string sharedFileName = sharedFile.fileName;
+
+                    if (importFilesHash.Contains(sharedFileName.ToUpper()))
+                    {
+                        continue;
+                    }
+
+                    if (!File.Exists(sharedFilePath))
+                    {
+                        string[] findFiles = Directory.GetFiles(Path.GetDirectoryName(fullName), sharedFileName, SearchOption.AllDirectories);
+                        if (findFiles.Length > 0)
+                        {
+                            sharedFilePath = findFiles[0];
+                        }
+                    }
+
+                    if (!File.Exists(sharedFilePath))
+                    {
+                        continue;
+                    }
+
+                    importFiles.Add(sharedFilePath);
+                    importFilesHash.Add(sharedFileName.ToUpper());
+
+                    value++;
+                }
+
+                if (value > 0)
+                {
+                    ProgressBarIncrementMaximum(value);
+                }
+            }
+            else
+            {
+                reader.Dispose();
             }
         }
 
         private static void LoadBundleFile(string fullName, EndianBinaryReader reader, string parentPath = null)
         {
-            var fileName = Path.GetFileName(fullName);
+            string fileName = Path.GetFileName(fullName);
+
             StatusStripUpdate("Decompressing " + fileName);
+
             var bundleFile = new BundleFile(reader, fullName);
             reader.Dispose();
-            foreach (var file in bundleFile.fileList)
+
+            foreach (StreamFile file in bundleFile.fileList)
             {
-                if (!assetsfileListHash.Contains(file.fileName.ToUpper()))
+                if (assetsfileListHash.Contains(file.fileName.ToUpper()))
                 {
-                    StatusStripUpdate("Loading " + file.fileName);
-                    var assetsFile = new AssetsFile(Path.GetDirectoryName(fullName) + "\\" + file.fileName, new EndianBinaryReader(file.stream));
-                    if (assetsFile.valid)
-                    {
-                        assetsFile.parentPath = parentPath ?? fullName;
+                    continue;
+                }
 
-                        if (assetsFile.header.m_Version == 6) //2.6.x and earlier don't have a string version before the preload table
-                        {
-                            //make use of the bundle file version
-                            assetsFile.unityVersion = bundleFile.versionEngine;
-                            assetsFile.version = Regex.Matches(bundleFile.versionEngine, @"\d").Cast<Match>().Select(m => int.Parse(m.Value)).ToArray();
-                            assetsFile.buildType = Regex.Replace(bundleFile.versionEngine, @"\d", "").Split(new[] { "." }, StringSplitOptions.RemoveEmptyEntries);
-                        }
+                StatusStripUpdate("Loading " + file.fileName);
 
-                        assetsfileList.Add(assetsFile);
-                        assetsfileListHash.Add(assetsFile.upperFileName);
-                    }
-                    else
+                var assetsFile = new AssetsFile(Path.GetDirectoryName(fullName) + "\\" + file.fileName, new EndianBinaryReader(file.stream));
+
+                if (assetsFile.valid)
+                {
+                    assetsFile.parentPath = parentPath ?? fullName;
+
+                    if (assetsFile.header.m_Version == 6) //2.6.x and earlier don't have a string version before the preload table
                     {
-                        resourceFileReaders.Add(assetsFile.upperFileName, assetsFile.reader);
+                        //make use of the bundle file version
+                        assetsFile.unityVersion = bundleFile.versionEngine;
+                        assetsFile.version = Regex.Matches(bundleFile.versionEngine, @"\d").Cast<Match>().Select(m => int.Parse(m.Value)).ToArray();
+                        assetsFile.buildType = Regex.Replace(bundleFile.versionEngine, @"\d", "").
+                            Split(new[]
+                            {
+                                "."
+                            }, StringSplitOptions.RemoveEmptyEntries);
                     }
+
+                    assetsFileList.Add(assetsFile);
+                    assetsfileListHash.Add(assetsFile.upperFileName);
+                }
+                else
+                {
+                    resourceFileReaders.Add(assetsFile.upperFileName, assetsFile.reader);
                 }
             }
         }
 
         private static void LoadWebFile(string fullName, EndianBinaryReader reader)
         {
-            var fileName = Path.GetFileName(fullName);
+            string fileName = Path.GetFileName(fullName);
+
             StatusStripUpdate("Loading " + fileName);
+
             var webFile = new WebFile(reader);
             reader.Dispose();
-            foreach (var file in webFile.fileList)
+
+            foreach (StreamFile file in webFile.fileList)
             {
-                var dummyName = Path.GetDirectoryName(fullName) + "\\" + file.fileName;
+                string dummyName = Path.GetDirectoryName(fullName) + "\\" + file.fileName;
+
                 switch (CheckFileType(file.stream, out reader))
                 {
                     case FileType.AssetsFile:
@@ -152,6 +188,7 @@ namespace AssetStudio
                         LoadWebFile(dummyName, reader);
                         break;
                 }
+
                 resourceFileReaders.Add(file.fileName.ToUpper(), reader);
             }
         }
@@ -159,22 +196,27 @@ namespace AssetStudio
         public static void MergeSplitAssets(string dirPath)
         {
             string[] splitFiles = Directory.GetFiles(dirPath, "*.split0");
-            foreach (var splitFile in splitFiles)
+
+            foreach (string splitFile in splitFiles)
             {
                 string destFile = Path.GetFileNameWithoutExtension(splitFile);
                 string destPath = Path.GetDirectoryName(splitFile) + "\\";
-                var destFull = destPath + destFile;
-                if (!File.Exists(destFull))
+                string destFull = destPath + destFile;
+
+                if (File.Exists(destFull))
                 {
-                    string[] splitParts = Directory.GetFiles(destPath, destFile + ".split*");
-                    using (var destStream = File.Create(destFull))
+                    continue;
+                }
+
+                string[] splitParts = Directory.GetFiles(destPath, destFile + ".split*");
+
+                using (FileStream destStream = File.Create(destFull))
+                {
+                    for (var i = 0; i < splitParts.Length; i++)
                     {
-                        for (int i = 0; i < splitParts.Length; i++)
-                        {
-                            string splitPart = destFull + ".split" + i;
-                            using (var sourceStream = File.OpenRead(splitPart))
-                                sourceStream.CopyTo(destStream);
-                        }
+                        string splitPart = destFull + ".split" + i;
+                        using (FileStream sourceStream = File.OpenRead(splitPart))
+                            sourceStream.CopyTo(destStream);
                     }
                 }
             }
